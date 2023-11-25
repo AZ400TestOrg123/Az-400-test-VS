458c6b3a97bf2344a68312a25e99ca41340d2ea3
Rashad Isayev <rashad.isayev@novum.tech>
2023-11-25 22:27:39 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Az_400_test_VS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
